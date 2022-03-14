bcbc50863ee5bd385ab145d82c07e842297a0b5b
ynn <1830084621@qq.com>
2022-03-14 23:41:03 +0800

    "PropTypes和ref使用方法"

--- my-app/src/Xiaojiejie.js
+++ my-app/src/Xiaojiejie.js
@@ -3,6 +3,8 @@ import './style.css'
 import XiaojiejieItem from "./XiaojiejieItem";
 
 class Xiaojiejie extends Component{
+    //生命周期函数指在某一个时刻组件会自动调用执行的函数
+
     constructor(props){
         super(props)
         this.state = {
@@ -10,16 +12,30 @@ class Xiaojiejie extends Component{
             list:['基础按摩', '精油推背']
         }
     }
+
+    componentWillMount() {
+        console.log('componentWillMount----组件将要挂载到页面的时刻');
+    }
+    componentDidMount() {
+        console.log('componentDidMount----组件挂载完成的时刻执行');
+    }
     render(){
+        console.log('render----组件加载中');
         return (
             //<div>
             <Fragment>
                 <div>
                     <label htmlFor="js">增加服务：</label>
-                    <input id = 'js' className = "input" value = {this.state.inputValue} onChange = {this.inputChange.bind(this)}/>
+                    <input 
+                        id = 'js' 
+                        className = "input" 
+                        value = {this.state.inputValue} 
+                        onChange = {this.inputChange.bind(this)}
+                        ref = {(input) => {this.input = input}}
+                    />
                     <button onClick={this.addList.bind(this)}>增加服务</button>
                 </div>
-                <ul>
+                <ul ref = {(ul) => {this.ul = ul}}>
                     {
                         this.state.list.map((item,index) => {
                             return (
@@ -35,6 +51,7 @@ class Xiaojiejie extends Component{
                                     XiaojiejieItem 
                                     key = {item + index}
                                     content = {item}
+                                   // avname = "某某"
                                     index = {index}
                                     deleteItem = {this.deleteItem.bind(this)}
 
@@ -48,19 +65,23 @@ class Xiaojiejie extends Component{
             //</div>
         )
     }
-    inputChange(e) {
+    inputChange() {
         //console.log(this);
         //console.log(e.target.value);
         //this.state.inputValue = e.target.value;（错误）
         this.setState({
-            inputValue : e.target.value
+            inputValue : this.input.value
         })
+
     }
     //增加服务选项
     addList() {
         this.setState({
             list : [...this.state.list, this.state.inputValue]
+        },() => {
+            console.log(this.ul.querySelectorAll('div').length);
         })
+
     }
     //删除单数服务
     deleteItem(index) {
--- my-app/src/XiaojiejieItem.js
+++ my-app/src/XiaojiejieItem.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class XiaojiejieItem extends Component {
     constructor(props) {
@@ -8,7 +9,7 @@ class XiaojiejieItem extends Component {
     render() { 
         return (
             <div onClick = {this.handleClick}>
-                {this.props.content}
+                {this.props.avname}为你服务- {this.props.content}
             </div>
         );
         }
@@ -16,5 +17,17 @@ class XiaojiejieItem extends Component {
         this.props.deleteItem(this.props.index)
     }
 }
+
+XiaojiejieItem.propTypes = {
+    avname : PropTypes.string.isRequired,
+    content : PropTypes.string,
+    index : PropTypes.number,
+    deleteItem : PropTypes.func,
+
+}
  
+
+XiaojiejieItem.defaultProps = {
+    avname : "很高兴"
+}
 export default XiaojiejieItem;
